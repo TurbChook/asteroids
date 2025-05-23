1cf2cf275c3faf0ab2e0ece8676fa4c9229c2c5d
TurbChook <john.criollo.a@gmail.com>
2025-05-23 19:11:55 +1000

    Adding player, circleshape, and printing the player

--- main.py
+++ main.py
@@ -3,15 +3,22 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+from circleshape import CircleShape
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)  
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,(1,1,1))
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
     #print("Starting Asteroids!")
     #print(f"Screen width: {SCREEN_WIDTH}")
     #print(f"Screen height: {SCREEN_HEIGHT}")
